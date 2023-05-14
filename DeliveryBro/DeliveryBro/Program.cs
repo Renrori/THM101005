4f4c5585eb0add4310681f588d562b51b707f511
Fred <fred22855@hotmail.com.tw>
2023-05-14 20:04:44 +0800

    Injection EF

--- DeliveryBro/DeliveryBro/Program.cs
+++ DeliveryBro/DeliveryBro/Program.cs
@@ -14,6 +14,7 @@ namespace DeliveryBro
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
+
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
             builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
