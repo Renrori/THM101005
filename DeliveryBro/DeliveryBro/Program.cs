5a0c33eefd2f92c6fadabb2fa7c0b4ae074201cc
Alli <yupm0524@gmail.com>
2023-05-16 11:00:07 +0800

    套版

--- DeliveryBro/DeliveryBro/Program.cs
+++ DeliveryBro/DeliveryBro/Program.cs
@@ -47,13 +47,16 @@ namespace DeliveryBro
 
             app.UseAuthentication();
             app.UseAuthorization();
-
+            app.MapControllerRoute(
+				name: "store",
+				pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             app.MapControllerRoute(name: "admin",
                 pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             app.MapControllerRoute(
                 name: "default",
                 pattern: "{controller=Home}/{action=Index}/{id?}");
-            app.MapRazorPages();
+			
+			app.MapRazorPages();
 
             app.Run();
         }
