d80dacd38ff70dc00a22c89b09b885fe308cbd29
Fred <fred22855@hotmail.com.tw>
2023-05-18 00:13:28 +0800

    function  return Store VM

--- DeliveryBro/DeliveryBro/Controllers/HomeController.cs
+++ DeliveryBro/DeliveryBro/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using DeliveryBro.Models;
+using DeliveryBro.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace DeliveryBro.Controllers
@@ -7,10 +9,12 @@ namespace DeliveryBro.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly sql8005site4nownetContext _context;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger , sql8005site4nownetContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
         public IActionResult Index()
@@ -18,9 +22,31 @@ namespace DeliveryBro.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public async Task<IActionResult> GetStore()
         {
-            return View();
+            var groupMenu = await _context.MenuTable
+               //.Where(m => !string.IsNullOrEmpty(m.DishName))
+               .GroupBy(m => m.DishId)
+               .Select(g => g.Key)
+               .ToListAsync();
+
+            var sm = await _context.RestaurantTable
+                .Where(s => groupMenu.Contains(s.RestaurantId))
+                .Select(s => new StoreViewModel
+                {
+                    StoreId = s.RestaurantId,
+                    StoreName = s.RestauranName,
+                    StorePicture = s.RestaurantPicture
+                })
+                .ToListAsync();
+            return Ok(sm);
+        }
+
+        public async Task<IActionResult> GetProduct(int id)
+        {
+            var product = _context.MenuTable.Include(m => m.Restaurant)
+               .Where(m => m.Restaurant.RestaurantId == id);
+            return Ok(product);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ DeliveryBro/DeliveryBro/ViewModels/StoreViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace DeliveryBro.ViewModels
+{
+    public class StoreViewModel
+    {
+        public int StoreId { get; set; }
+        public string StoreName { get; set; }
+        public byte[] StorePicture { get; set; }
+    }
+}
