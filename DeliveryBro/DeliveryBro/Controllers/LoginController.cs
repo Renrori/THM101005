32f25b79fe742fd980bd3404e8755d604915d19c
Libetsai <a0910032300@gmail.com>
2023-05-21 12:18:26 +0800

    no message

--- DeliveryBro/DeliveryBro/Controllers/LoginController.cs
+++ DeliveryBro/DeliveryBro/Controllers/LoginController.cs
@@ -3,13 +3,13 @@ using DeliveryBro.ViewModels.User;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DeliveryBro.Controllers
-{   
-    
+{
+
     public class LoginController : Controller
     {
         private readonly sql8005site4nownetContext _context;
 
-        public LoginController(sql8005site4nownetContext context) 
+        public LoginController(sql8005site4nownetContext context)
         {
             _context = context;
         }
@@ -19,20 +19,20 @@ namespace DeliveryBro.Controllers
         }
         public IActionResult SignUp()
         {
-            return View();        
-           
-        //public Task<IActionResult> Login(LoginViewModel lm)
-        //{
-        //    //比對是否存在此ID用戶
-        //    var user = _context.CustomersTable.FirstOrDefault(x => x.CustomerId == lm.UserID && x.CustomerPassword == lm.UserPassword);
-        //    var loginTry = 0;
-        //    if (user != null)
-        //    {
+            return View();
+
+            //public Task<IActionResult> Login(LoginViewModel lm)
+            //{
+            //    //比對是否存在此ID用戶
+            //    var user = _context.CustomersTable.FirstOrDefault(x => x.CustomerId == lm.UserID && x.CustomerPassword == lm.UserPassword);
+            //    var loginTry = 0;
+            //    if (user != null)
+            //    {
 
 
-        //    }
+            //    }
 
-        //    return View();
-        //}
+            //    return View();
+            //}
+        }
     }
-}
