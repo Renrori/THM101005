84ccb2825acc297381c567bed4ada2ae757271e4
Alli <yupm0524@gmail.com>
2023-05-23 20:28:16 +0800

    菜單頁新刪修

---
+++ DeliveryBro/DeliveryBro/Areas/store/DTO/HisOrderDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DeliveryBro.Areas.store.DTO
+{
+	public class HisOrderDTO
+	{
+
+			public int OrderId { get; set; }
+			public DateTime OrderDate { get; set; }
+			//public string DishName { get; set; }
+			public string CustomerName { get; set; }
+			public string Note { get; set; }
+			public int Total { get; set; }
+		 public ICollection<OrderDetailsDTO> OrderDetails { get; set; }
+    }
+}
---
+++ DeliveryBro/DeliveryBro/Areas/store/DTO/OrderDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace DeliveryBro.Areas.store.DTO
+{
+    public class OrderDetailsDTO
+    {
+        //public int OrderId { get; set; }
+        //public DateTime OrderDate { get; set; }
+        public string DishName { get; set; }
+        public int UnitPrice { get; set; }
+        public int Quantity { get; set; }
+        public int? Discount { get; set; }
+        public int Subtotal { get; set; }
+        
+    }
+}
