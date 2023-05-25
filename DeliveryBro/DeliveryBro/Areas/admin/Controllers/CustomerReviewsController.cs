3cf027af19f75d2c75c8c5d19cca9236cb721e49
路小咩的電腦\jerry <jerrywang704@gmail.com>
2023-05-26 00:11:39 +0800

    新增訂單,用戶,用戶評價的view controllers

---
+++ DeliveryBro/DeliveryBro/Areas/admin/Controllers/CustomerReviewsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DeliveryBro.Areas.admin.Controllers
+{
+    public class CustomerReviewsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
