d098de5a49485c82df59856f757b445a8f049c77
Fred <fred22855@hotmail.com.tw>
2023-05-26 14:23:50 +0800

    新增登入控制器，對應頁面

--- DeliveryBro/DeliveryBro/Controllers/LoginController.cs
+++ DeliveryBro/DeliveryBro/Controllers/LoginController.cs
@@ -1,6 +1,7 @@
 ﻿using DeliveryBro.Models;
-using DeliveryBro.ViewModels.User;
+using DeliveryBro.ViewModels.Login;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace DeliveryBro.Controllers
 {
@@ -13,27 +14,32 @@ namespace DeliveryBro.Controllers
         {
             _context = context;
         }
-        public IActionResult Login()
+        public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index([Bind("UserAccount, UserPassword")]LoginViewModel login)
+        {
+            if (ModelState.IsValid)
+            {
+                //比對是否存在此ID用戶
+                var user = _context.CustomersTable.FirstOrDefault(x => x.CustomerAccount == login.UserAccount && x.CustomerPassword == login.UserPassword);
+                if (user == null)
+                {
+                    ViewBag.ErrorMessage = "找不到此帳號!";
+                    return View(login);
+                }
+
+            }
+            return RedirectToAction("Index", "Home");
         }
         public IActionResult SignUp()
         {
             return View();
 
-            //public Task<IActionResult> Login(LoginViewModel lm)
-            //{
-            //    //比對是否存在此ID用戶
-            //    var user = _context.CustomersTable.FirstOrDefault(x => x.CustomerId == lm.UserID && x.CustomerPassword == lm.UserPassword);
-            //    var loginTry = 0;
-            //    if (user != null)
-            //    {
-
-
-            //    }
-
-            //    return View();
-            //}
         }
     }
 }
