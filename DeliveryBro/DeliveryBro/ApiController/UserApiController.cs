408c26d6c5f426c177f68f117f58f939799811b8
Fred <fred22855@hotmail.com.tw>
2023-05-28 17:55:51 +0800

    取身分驗證的內容來查使用者資料

--- DeliveryBro/DeliveryBro/ApiController/UserApiController.cs
+++ DeliveryBro/DeliveryBro/ApiController/UserApiController.cs
@@ -1,9 +1,12 @@
 ﻿using DeliveryBro.Models;
 using DeliveryBro.ViewModels.User;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using Microsoft.EntityFrameworkCore;
 
 namespace DeliveryBro.ApiController
@@ -20,30 +23,44 @@ namespace DeliveryBro.ApiController
             _context = context;
         }
 
+        //[Authorize]
+        ////GET:api/UserApi/2
+        //[HttpGet]
+        ////[Authorize(Role="User")] 限制登入者?
+        //public async Task<UserInfoViewModel> GetUserInfo()
+        //{
+        //    var login = await HttpContext.AuthenticateAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        //    if (login.Succeeded)
+        //    {
+        //        var userIdClaim = login.Principal.Claims.FirstOrDefault(c=>c.ValueType== "customerId");
+        //        var userId = userIdClaim?.Value;
+        //        var user = await _context.CustomersTable.Include(c => c.CustomerAddressTable)
+        //        .FirstOrDefaultAsync(c => c.CustomerId == userId);
+        //    }
+            
 
-        //GET:api/UserApi/2
-        [HttpGet("{customerId}")]
-        //[Authorize(Role="User")] 限制登入者?
-        public async Task<UserInfoViewModel> GetUserInfo(int customerId)
-        {
-            var user = await _context.CustomersTable.FindAsync(customerId);
-            UserInfoViewModel userInfo = new UserInfoViewModel
-            {
-                UserId =user.CustomerId,
-                UserName = user.CustomerName,
-                UserEmail = user.CustomerEmail,
-                UserBirth = user.DateOfBirth,
-                UserPhone =user.CustomerPhone,
-                UserPicture = user.CustomerPhoto
-     
-            };
-            return userInfo;
-        }
+        //    var userAddress = await _context.CustomerAddressTable.Where(c => c.CustomerId == customerId).Select(c => c.CustomerAddress).ToListAsync();
+
+        //    var userInfo = new UserInfoViewModel
+        //    {
+
+        //        UserId = user.CustomerId,
+        //        UserName = user.CustomerName,
+        //        UserEmail = user.CustomerEmail,
+        //        UserAddress = userAddress,
+        //        UserBirth = user.DateOfBirth,
+        //        UserPhone = user.CustomerPhone,
+        //        UserPicture = user.CustomerPhoto
+
+        //    };
+        //    return userInfo;
+        //}
 
         //Put:api/UserApi/4
         [HttpPut("{customerId}")]
         public async Task<string> EditUserInfo(int customerId, EditUserInfoViewModel eui)
         {
+            
 
             //Httpcontext
             CustomersTable userInfo = await _context.CustomersTable.FindAsync(customerId);
@@ -54,6 +71,7 @@ namespace DeliveryBro.ApiController
             userInfo.CustomerPhone = eui.UserPhone;
             userInfo.CustomerPhoto = eui.UserPicture;
 
+            //track追蹤改變者上傳
             _context.Entry(userInfo).State = EntityState.Modified;
 
             try
--- DeliveryBro/DeliveryBro/ViewModels/User/UserInfoViewModel.cs
+++ DeliveryBro/DeliveryBro/ViewModels/User/UserInfoViewModel.cs
@@ -5,6 +5,7 @@
         public int UserId { get; set; }
         public string UserName { get; set;}
         public string? UserEmail { get; set;}
+        public ICollection<string> UserAddress { get; set;}
         public DateTime? UserBirth {get; set;}
         public string? UserPhone { get; set;}
         public byte[]? UserPicture { get; set;}
