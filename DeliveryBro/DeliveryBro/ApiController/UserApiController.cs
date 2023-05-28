84253fee26a2a1ae996ba508e8ebd2bd3c647161
Fred <fred22855@hotmail.com.tw>
2023-05-28 08:40:03 +0800

    新增控制器取得消費者修改個人資料

---
+++ DeliveryBro/DeliveryBro/ApiController/UserApiController.cs
@@ -0,0 +1,53 @@
+﻿using DeliveryBro.Models;
+using DeliveryBro.ViewModels.User;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryBro.ApiController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserApiController : ControllerBase
+    {
+        private readonly sql8005site4nownetContext _context;
+
+        public UserApiController(sql8005site4nownetContext context)
+        {
+            _context = context;
+        }
+
+
+        //GET:api/UserApi/2
+        [HttpGet("{customerId}")]
+        public async Task<UserInfoViewModel> GetUserInfo(int customerId)
+        {
+            var user = await _context.CustomersTable.FindAsync(customerId);
+            UserInfoViewModel userInfo = new UserInfoViewModel
+            {
+                UserId =user.CustomerId,
+                UserName = user.CustomerName,
+                UserEmail = user.CustomerEmail,
+                UserBirth = user.DateOfBirth,
+                UserPhone =user.CustomerPhone,
+                UserPicture = user.CustomerPhoto
+     
+            };
+            return userInfo;
+        }
+
+        //[HttpPost("{customerId}")]
+        //public async Task<string> EditUserInfo(int customerId, EditUserInfoViewModel eui)
+        //{
+        //    if (customerId != eui.UserId)
+        //    {
+        //        return "資料不符合";
+        //    }
+        //    //Httpcontext
+        //    CustomersTable cus = await _context.CustomersTable.FindAsync(customerId);
+        //    {
+               
+        //    }
+        //}
+    }
+}
---
+++ DeliveryBro/DeliveryBro/ViewModels/User/UserInfoViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace DeliveryBro.ViewModels.User
+{
+    public class UserInfoViewModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set;}
+        public string? UserEmail { get; set;}
+        public DateTime? UserBirth {get; set;}
+        public string? UserPhone { get; set;}
+        public byte[]? UserPicture { get; set;}
+
+    }
+}
