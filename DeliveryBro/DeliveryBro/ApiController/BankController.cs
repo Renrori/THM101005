28c6964348d4896f6e138263f32cdf8c87513bc7
Libetsai <a0910032300@gmail.com>
2023-06-03 20:02:36 +0800

    金流串接出版，尚未完成

---
+++ DeliveryBro/DeliveryBro/ApiController/BankController.cs
@@ -0,0 +1,146 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using DeliveryBro.Models.bankModel;
+using DeliveryBro.Utility;
+
+namespace DeliveryBro.ApiController
+{
+    public class BankController : Controller
+    {
+        private BankInfoModel _bankInfoModel = new BankInfoModel
+        {
+            MerchantID = "MS149059134",
+            HashKey = "MKSORT4d0ti9JkiDhRQU5keB1l3EQ4Cr",
+            HashIV = "C8OZkhuhY98SdP3P",
+            ReturnURL = "http://yourWebsitUrl/Bank/SpgatewayReturn",
+            NotifyURL = "http://yourWebsitUrl/Bank/SpgatewayNotify",
+            CustomerURL = "http://yourWebsitUrl/Bank/SpgatewayCustomer",
+            AuthUrl = "https://ccore.newebpay.com/MPG/mpg_gateway",
+            CloseUrl = "https://core.newebpay.com/API/CreditCard/Close"
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task SpgatewayPayBillAsync(string ordernumber, int amount, string payType)
+        {
+            string version = "1.5";
+
+            // 目前時間轉換 +08:00, 防止傳入時間或Server時間時區不同造成錯誤
+            DateTimeOffset taipeiStandardTimeOffset = DateTimeOffset.Now.ToOffset(new TimeSpan(8, 0, 0));
+
+            TradeInfo tradeInfo = new TradeInfo()
+            {
+                // * 商店代號
+                MerchantID = _bankInfoModel.MerchantID,
+                // * 回傳格式
+                RespondType = "String",
+                // * TimeStamp
+                TimeStamp = taipeiStandardTimeOffset.ToUnixTimeSeconds().ToString(),
+                // * 串接程式版本
+                Version = version,
+                // * 商店訂單編號
+                //MerchantOrderNo = $"T{DateTime.Now.ToString("yyyyMMddHHmm")}",
+                MerchantOrderNo = ordernumber,
+                // * 訂單金額
+                Amt = amount,
+                // * 商品資訊
+                ItemDesc = "商品資訊(自行修改)",
+                // 繳費有效期限(適用於非即時交易)
+                ExpireDate = null,
+                // 支付完成 返回商店網址
+                ReturnURL = _bankInfoModel.ReturnURL,
+                // 支付通知網址
+                NotifyURL = _bankInfoModel.NotifyURL,
+                // 商店取號網址
+                CustomerURL = _bankInfoModel.CustomerURL,
+                // 支付取消 返回商店網址
+                ClientBackURL = null,
+                // * 付款人電子信箱
+                Email = "reds1210@gmail.com",
+                // 付款人電子信箱 是否開放修改(1=可修改 0=不可修改)
+                EmailModify = 0,
+                // 商店備註
+                OrderComment = "xxxxx",
+                // 信用卡 一次付清啟用(1=啟用、0或者未有此參數=不啟用)
+                CREDIT = 1,
+                // WEBATM啟用(1=啟用、0或者未有此參數，即代表不開啟)
+                WEBATM = 1,
+                // ATM 轉帳啟用(1=啟用、0或者未有此參數，即代表不開啟)
+                VACC = 1,
+                // 超商代碼繳費啟用(1=啟用、0或者未有此參數，即代表不開啟)(當該筆訂單金額小於 30 元或超過 2 萬元時，即使此參數設定為啟用，MPG 付款頁面仍不會顯示此支付方式選項。)
+                CVS = null,
+                // 超商條碼繳費啟用(1=啟用、0或者未有此參數，即代表不開啟)(當該筆訂單金額小於 20 元或超過 4 萬元時，即使此參數設定為啟用，MPG 付款頁面仍不會顯示此支付方式選項。)
+                BARCODE = null,
+
+            };
+
+            if (string.Equals(payType, "CREDIT"))
+            {
+                tradeInfo.CREDIT = 1;
+            }
+            else if (string.Equals(payType, "WEBATM"))
+            {
+                tradeInfo.WEBATM = 1;
+            }
+            else if (string.Equals(payType, "VACC"))
+            {
+                // 設定繳費截止日期
+                tradeInfo.ExpireDate = taipeiStandardTimeOffset.AddDays(1).ToString("yyyyMMdd");
+                tradeInfo.VACC = 1;
+            }
+            else if (string.Equals(payType, "CVS"))
+            {
+                // 設定繳費截止日期
+                tradeInfo.ExpireDate = taipeiStandardTimeOffset.AddDays(1).ToString("yyyyMMdd");
+                tradeInfo.CVS = 1;
+            }
+            else if (string.Equals(payType, "BARCODE"))
+            {
+                // 設定繳費截止日期
+                tradeInfo.ExpireDate = taipeiStandardTimeOffset.AddDays(1).ToString("yyyyMMdd");
+                tradeInfo.BARCODE = 1;
+            }
+
+            Atom<string> result = new Atom<string>()
+            {
+                IsSuccess = true
+            };
+
+            var inputModel = new SpgatewayInputModel
+            {
+                MerchantID = _bankInfoModel.MerchantID,
+                Version = version
+            };
+
+            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            List<KeyValuePair<string, string>> tradeData = LambdaUtil.ModelToKeyValuePairList<TradeInfo>(tradeInfo);
+            // 將List<KeyValuePair<string, string>> 轉換為 key1=Value1&key2=Value2&key3=Value3...
+            var tradeQueryPara = string.Join("&", tradeData.Select(x => $"{x.Key}={x.Value}"));
+            // AES 加密
+            inputModel.TradeInfo = CryptoUtil.EncryptAESHex(tradeQueryPara, _bankInfoModel.HashKey, _bankInfoModel.HashIV);
+            // SHA256 加密
+            inputModel.TradeSha = CryptoUtil.EncryptSHA256($"HashKey={_bankInfoModel.HashKey}&{inputModel.TradeInfo}&HashIV={_bankInfoModel.HashIV}");
+
+            // 將model 轉換為List<KeyValuePair<string, string>>, null值不轉
+            List<KeyValuePair<string, string>> postData = LambdaUtil.ModelToKeyValuePairList<SpgatewayInputModel>(inputModel);
+
+            Response.Clear();
+
+            StringBuilder s = new StringBuilder();
+            s.Append("<html>");
+            s.AppendFormat("<body onload='document.forms[\"form\"].submit()'>");
+            s.AppendFormat("<form name='form' action='{0}' method='post'>", _bankInfoModel.AuthUrl);
+            foreach (KeyValuePair<string, string> item in postData)
+            {
+                s.AppendFormat("<input type='hidden' name='{0}' value='{1}' />", item.Key, item.Value);
+            }
+
+            s.Append("</form></body></html>");
+            byte[] bytes = Encoding.ASCII.GetBytes(s.ToString());
+            await HttpContext.Response.Body.WriteAsync(bytes);
+        }
+    }
+}
---
+++ DeliveryBro/DeliveryBro/Models/bankModel/SpgatewayInputModel.cs
@@ -0,0 +1,26 @@
+﻿namespace DeliveryBro.Models.bankModel
+{
+    public class SpgatewayInputModel
+    {
+
+        /// <summary>
+        /// 商店代號
+        /// </summary>
+        public string MerchantID { get; set; }
+
+        /// <summary>
+        /// 交易資料AES 加密
+        /// </summary>
+        public string TradeInfo { get; set; }
+
+        /// <summary>
+        /// 交易資料SHA256 加密
+        /// </summary>
+        public string TradeSha { get; set; }
+
+        /// <summary>
+        /// 串接程式版本
+        /// </summary>
+        public string Version { get; set; }
+    }
+}
