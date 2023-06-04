472afb42ecdb7242a4cd9269ca78969ec33e0a1f
路小咩的電腦\jerry <jerrywang704@gmail.com>
2023-06-04 18:19:44 +0800

    add order api

---
+++ DeliveryBro/DeliveryBro/Areas/admin/Controllers/ApiControllers/OrderManagementController.cs
@@ -0,0 +1,37 @@
+﻿using DeliveryBro.Areas.store.DTO;
+using DeliveryBro.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeliveryBro.Areas.admin.Controllers.ApiControllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class OrderManagementController : ControllerBase
+    {
+        private readonly sql8005site4nownetContext _db;
+        public OrderManagementController(sql8005site4nownetContext context)
+        {
+            _db = context;
+        }
+
+        ///api/OrderManagement/All <summary>
+        [Route("all")]
+        [HttpGet]
+        public async Task<IActionResult> All()
+        {
+            var r = await _db.CustomerOrderTable.Select(x => new
+            {
+                OrderID = x.OrderId,
+                OrderDate = x.OrderDate.ToUniversalTime().ToLocalTime().ToString(),
+                CustomerAdd = x.CustomerAddress,
+                AmountAfterDiscount = x.AmountAfterDiscount,
+                OrderStatus = x.OrderStatus,
+            }).ToListAsync();
+
+            return Ok(r);
+        }
+    }
+}
